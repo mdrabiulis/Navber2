43f5e6af6164d6ad777c9268e2d27433b22c8682
mdrabiulis <mdrabiulis155@gmail.com>
2023-12-21 03:54:55 +0600

    add navbar css file

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,12 +1,22 @@
 import Log from "./Log/Log";
-import "./navbar.css"
+import "./navbar.css";
 
 const Navbar = () => {
-    return (
-        <div className="">
-            <Log ></Log>
-        </div>
-    );
+  return (
+    <div className="navBar">
+      <Log></Log>
+      <nav className="">
+        <ul className="">
+          <li className="hover:bg-red-300 w-20 rounded-md p-2">Home</li>
+          <li className="hover:bg-red-300 w-20 rounded-md p-2">Blog</li>
+          <li className="hover:bg-red-300 w-20 rounded-md p-2">About</li>
+          <li className="hover:bg-red-300 w-20 rounded-md p-2">Contact</li>
+          <li className="hover:bg-red-300 w-20 rounded-md p-2">Utility</li>
+          <li className="hover:bg-red-300 w-20 rounded-md p-2 hover:drop-shadow-md ">Account</li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
